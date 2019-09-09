03de54eca0f0c9eaf7218ad0800226ee7ede56c1
hienlh <hienlh1298@gmail.com>
2019-09-09 16:00:25 +0700

    Change render image by support server

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,11 +2,12 @@
 
 namespace OmgGame\Http\Controllers;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
 use OmgGame\Models\Game;
 use OmgGame\Models\GameResult;
-use function MongoDB\BSON\toJSON;
 
 class ApiController extends Controller
 {
@@ -32,57 +33,19 @@ class ApiController extends Controller
             ->where('delete_at', null);
         $index = array_rand($result->toArray());
         $image_url = $result[$index]->image;
-        $design_json = json_decode($result[$index]->design);
-
-        $all_objs = $design_json->children[0]->children;
-        $background_obj = null;
-        $avatar_obj = null;
-        $name_obj = null;
-        $text_list_objs = [];
-        foreach ($all_objs as $obj) {
-            if (isset($obj->attrs->id) && $obj->attrs->id == 'background') {
-                $background_obj = $obj->attrs;
-            } else if (isset($obj->attrs->id) && $obj->attrs->id == 'avatar') {
-                $avatar_obj = $obj->attrs;
-            } else if (isset($obj->attrs->id) && $obj->attrs->id == 'name') {
-                $name_obj = $obj->attrs;
-            } else if (isset($obj->className) && $obj->className == 'Text') {
-                array_push($text_list_objs, $obj->attrs);
-            }
+         $client= new Client();
+        try {
+            return $client->post('https://omg-support-server.herokuapp.com', [
+                'form_params' => [
+                    'json' => $result[$index]->design,
+                    'background' => $image_url,
+                    'avatar' => $request->avatar,
+                    'name' => $request->name
+                ]
+            ]);
+        } catch (GuzzleException $e) {
+            return $game_id;
         }
-
-        // Render background
-        $image = Image::make($image_url)->resize($background_obj->width, $background_obj->height);
-
-        // Render avatar
-        $avatar_image = Image::make($request->avatar)->resize(
-            (isset($avatar_obj->width) ? $avatar_obj->width : 200) * (isset($avatar_obj->scaleX) ? $avatar_obj->scaleX : 1),
-            (isset($avatar_obj->height) ? $avatar_obj->height : 200) * (isset($avatar_obj->scaleY) ? $avatar_obj->scaleY : 1));
-        $image = $image->insert($avatar_image, 'top-left', isset($avatar_obj->x) ? (int)$avatar_obj->x : 0,
-            isset($avatar_obj->y) ? (int)$avatar_obj->y : 0);
-
-        // Render name
-        $image->text($request->name,
-            isset($name_obj->x) ? (int)($name_obj->x + $name_obj->width / 2) : 0,
-            isset($name_obj->y) ? (int)$name_obj->y : 0,
-            function ($font) {
-                $font->file(public_path('admin/fonts/arial.ttf'));
-                $font->size(isset($text_obj->fontSize) ? $text_obj->fontSize : 20);
-                $font->align('center');
-            });
-
-        // Render other text
-        foreach ($text_list_objs as $text_obj) {
-            $image->text(isset($text_obj->text) ? $text_obj->text : '',
-                isset($text_obj->x) ? (int)($text_obj->x + $text_obj->width / 2) : 0,
-                isset($text_obj->y) ? (int)$text_obj->y : 0, function ($font) {
-                    $font->file(public_path('admin/fonts/arial.ttf'));
-                    $font->size(isset($text_obj->fontSize) ? $text_obj->fontSize : 20);
-                });
-        }
-
-        return $image->encode('data-url');
-//        return public_path('admin/fonts/arial.ttf');
     }
 
     public function test()
--- resources/views/fb_login_success.blade.php
+++ resources/views/fb_login_success.blade.php
@@ -1,16 +1,37 @@
-<!doctype html>
+{{-- <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <title>Facebook Login Success</title>
     <script>
         window.onload = () => {
-            document.location = 'https://loginsuccess';
+            console.log('login success');
+            // parent.WebviewManager.loadScene()
+            document.location = 'omggame://loginsuccess';
         }
     </script>
 </head>
 <body>
 </body>
+</html> --}}
+
+<html>
+<body>
+    <dev>
+        <input type="button" value="Trigger" onclick="onClick()"/>
+        <input type="button" value="Function" onclick="func()"/>
+    </dev>
+</body>
+<script>
+    function onClick () {
+        // One of them sets up the URL scheme
+        document.location = 'omggame://loginsuccess&a=1&b=2';
+    }
+    function func () {
+        parent.loadScene();
+    }
+</script>
 </html>
