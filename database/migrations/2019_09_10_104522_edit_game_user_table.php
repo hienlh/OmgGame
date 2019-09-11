ea373a8be140ddf1e231e6acf768f2c9a19ebb5c
hienlh <hienlh1298@gmail.com>
2019-09-11 15:39:42 +0700

    Update migrate

--- database/migrations/2019_09_10_104522_edit_game_user_table.php
+++ database/migrations/2019_09_10_104522_edit_game_user_table.php
@@ -13,15 +13,6 @@ class EditGameUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_play_game', function (Blueprint $table) {
-            $table->string('game_user_id', 50);
-            $table->bigInteger('game_id')->unsigned()->index();
-
-            $table->primary(['game_user_id', 'game_id']);
-            $table->foreign('game_user_id')->references('id')->on('game_users')->onDelete('cascade');
-            $table->foreign('game_id')->references('id')->on('games')->onDelete('cascade');
-        });
-
         Schema::table('game_users', function (Blueprint $table) {
             $table->dropForeign('game_users_game_id_foreign');
             $table->dropColumn('game_id');
@@ -31,6 +22,15 @@ class EditGameUserTable extends Migration
 
         Schema::table('extra_infos', function (Blueprint $table) {
             $table->string('game_user_id', 50)->change();
+        });
+
+        Schema::create('user_play_game', function (Blueprint $table) {
+            $table->string('game_user_id', 50);
+            $table->bigInteger('game_id')->unsigned()->index();
+
+            $table->primary(['game_user_id', 'game_id']);
+            $table->foreign('game_user_id')->references('id')->on('game_users')->onDelete('cascade');
+            $table->foreign('game_id')->references('id')->on('games')->onDelete('cascade');
         });
     }
 
